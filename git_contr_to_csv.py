91834f890f44cfac1e90e4012cf12d63e54d4255
bpankowski <bartosz.pankowski@codilime.com>
2019-01-11 14:09:26 +0100

    Cleanup of code.
    Change of variables names.
    Change of displayed informations.
    Change of while statement.
    Change api string building.

--- git_contr_to_csv.py
+++ git_contr_to_csv.py
@@ -4,9 +4,9 @@ import ast
 import csv
 
 contr = {}
-token = open("token", "r")
-token = token.read()
-repoItem = {}
+git_token = open("token", "r")
+git_token = git_token.read()
+repo_response = {}
 x = 0
 z = 0
 repos = []
@@ -14,31 +14,23 @@ with open("repos_list") as file:
     for line in file: 
         line = line.strip()
         repos.append(line)
-while True:
-     api = 'https://api.github.com/repos/' + repos[z] + '/commits?per_page=100&page=' + str(x) + '&access_token=' + token
+while z != len(repos):
+     api = 'https://api.github.com/repos/{repository}/commits?per_page=100&page={pageNum}&access_token={gitToken}'.format(repository=repos[z], gitToken=git_token, pageNum=str(x))
      x += 1
-     print api
+     print "repository: {repository} page: {pageNum}".format(pageNum=x, repository=repos[z])
      r = requests.get(api)
      if(r.ok):
-          repoItem = json.loads(r.text or r.content)
-     if not repoItem:
+          repo_response = json.loads(r.text or r.content)
+     if not repo_response:
           print "end of list."
           z += 1
           x = 0
-     for y in repoItem:
+     for y in repo_response:
           if y['commit']['author']['email'] in contr:
                contr[y['commit']['author']['email']] += 1
           else:
-               contr[y['commit']['author']['email']] = 1
-          print contr
-     print x
-     # with open('dict.txt', 'w') as f:
-     #      f.write(str(contr))
-     # with open('dict.txt', "r") as data:
-     #      dict1 = ast.literal_eval(data.read())
+               contr[y['commit']['author']['email']] = 1     
      with open('output.csv', 'wb') as output:
           writer = csv.writer(output)
           for key, value in contr.iteritems():
                writer.writerow([key, value])
-     if z == len(repos):
-          break
